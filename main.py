df128ae7b6f68c623b09a029a58c749c447c01a2
LuvLetter <1038875691@qq.com>
2022-08-20 14:12:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ def get_page():
     response = requests.get(url, headers=headers)
     response.encoding = 'gbk'
     html = response.text
-    page_number = int(re.findall('page">共(.*?)页', html)[0]) + 1  # 获取已封禁用户页数
-    get_id()
+    page_number = int(re.findall('page">共(.*?)页', html)[0]) + 1
+    get_id(url)
 
-def get_id():
-    for i in range(1, 3):  # 循环获取已封禁用户列表
-        url = 'https://tieba.baidu.com/bawu2/platform/listUserLog?stype=op_uname&svalue=%DA%A4%CB%BC%BE%B2%D7%F8&begin=&end=&op_type=&word=%BB%DD%B6%AB&pn=' + str(i)  # 贴吧用户管理页面
+def get_id(url):
+    for i in range(1, 3):  # 查询页数根据需求自行调整
+        url = url.replace('&pn=1', '&pn=') + str(i)
         response = requests.get(url, headers=headers)
         response.encoding = 'gbk'
         html = response.text
-        ban_info = re.findall('<img class="portrait".*?/item/(.*?)\' />                                    (.*?)</a>.*?此处输入小吧管理百度用户名</a></td><td>(.*?)</td></tr>', html)  # 获取用户 id、用户名、封禁时间
+        ban_info = re.findall('<img class="portrait".*?/item/(.*?)\' />                                    (.*?)</a>.*?管理员用户名</a></td><td>(.*?)</td></tr>', html)
         with open('user.txt') as f:
-            user_data = f.readlines()  # 白名单用户
+            user_data = f.readlines()
             user_text = str(user_data)
         f.close()
-        if os.path.getsize('blacklist.json') == 0:  # 初次使用直接写入数据
+        if os.path.getsize('blacklist.json') == 0:
             for item in ban_info:
                 if item[0] in user_text:
                     pass
@@ -35,18 +35,18 @@ def get_id():
         else:
             with open('blacklist.json') as f:
                 json_data = json.load(f)
-                json_text = str(json_data)  # 黑名单数据
+                json_text = str(json_data)
             f.close()
-            for k in list(json_data.keys()):  # 删除在黑名单内的白名单用户
+            for k in list(json_data.keys()):
                 if k in user_text:
                     del json_data[k]
             for item in ban_info:
-                if item[0] in user_text or item[0] in json_text:  # 检查新数据否在黑名单或白名单内
+                if item[0] in user_text or item[0] in json_text:
                     pass
                 else:
                     ban_data[item[0]] = {'user_name': item[1], 'ban_time': item[2]}
             json_data.update(ban_data)
-    with open('blacklist.json', 'w') as f:  # 未在记录用户写入文件
+    with open('blacklist.json', 'w') as f:
         f.write(json.dumps(json_data, indent=4, ensure_ascii=False))
     f.close()
     ban_id(ban_data)
@@ -54,7 +54,7 @@ def get_id():
 def ban_id(ban_data):
     true_msg = 0
     error_msg = 0
-    if os.path.getsize('user.txt') == 0:  #判断白名单是否为空
+    if os.path.getsize('user.txt') == 0:
         pass
     else:
         with open('user.txt', encoding='utf-8') as f:
@@ -65,10 +65,10 @@ def ban_id(ban_data):
                         del ban_data[line]
                         del json_all[line]
         f.close()
-    with open('blacklist.json') as f:  # 获取黑名单数据
+    with open('blacklist.json') as f:
             json_data = json.load(f)
     f.close()
-    for k, v in json_data.items():  # 遍历 json
+    for k, v in json_data.items():
         url = 'https://tieba.baidu.com/pmc/blockid'
         payload = {
             'day': 1,  # 封禁天数默认 1 天
@@ -89,11 +89,11 @@ def ban_id(ban_data):
     print('本次共封禁：%s用户,封禁失败%s用户' % (true_msg, error_msg))
 
 if __name__ == '__main__':
-    if not os.path.isfile('tieba.config'):  # 判断配置文件是否存在
+    if not os.path.isfile('tieba.config'):
         f = open('tieba.config', 'w', encoding='utf-8')
         f.write('fid=\ntbs=\nCookies=')
         f.close()
-    elif not os.path.isfile('blacklist.json'):  # 检查黑名单文件是否存在
+    elif not os.path.isfile('blacklist.json'):
         f = open('blacklist.json', 'w')
         f.close()
     elif not os.path.isfile('user.txt'):
@@ -113,10 +113,10 @@ if __name__ == '__main__':
                     elif 'Cookies' in line:
                         Cookies = lines.replace('Cookies=', '')
     ban_data = {}
-    url = 'https://tieba.baidu.com/bawu2/platform/listUserLog?word=%BB%DD%B6%AB'
+    url = '贴吧用户管理第一页地址'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
-        'Referer': 'https://tieba.baidu.com/bawu2/platform/listPostLog?stype=&svalue=&begin=&end=&op_type=&word=%BB%DD%B6%AB&pn=8',
+        'Referer': '贴吧主页地址',
         'Cookie': Cookies
     }
     get_page()
